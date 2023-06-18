c098885b073442454fe052ea1c9b2bff69c70247
iweinacker <>
2023-06-18 13:52:40 +0000

    Adding the question of the game

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
 from question import Question as qt
 
+question_quiz = [
+    "1. What kind of fish is Nemo? /n A) Shark/n B) Clownfish/n C) Whale/n /n",
+    "2. Who is Ariel’s best friend in the Little Mermaid?? /n A) Spongebob/n B) Poseidon/n C)  Flounder/n /n",
+    "3. Which school did Harry Potter attend? /n A) Hoggwart/n B) Harvart/n C) Homeschool/n /n",
+    "4. What kind of animal is Simba /n A) Dog/n B) Tiger/n C) Lion/n /n",
+    "5. What is Shrek?? /n A) An ogre/n B) A prince/n C) A donkey/n /n",
+]
